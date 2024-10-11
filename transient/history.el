3492f8f4ef77caa29a3b82cbb656261556408b17
No Soy Anonimo <enemigosdekarmatron@gmail.com>
2024-10-11 10:48:50 -0600

    Primer commit

---
+++ transient/history.el
@@ -0,0 +1,10 @@
+((magit-cherry-pick
+  ("--ff"))
+ (magit-commit nil)
+ (magit-dispatch nil)
+ (magit-merge nil)
+ (magit-pull nil)
+ (magit-push nil)
+ (magit-remote
+  ("-f"))
+ (magit-status-jump nil))
